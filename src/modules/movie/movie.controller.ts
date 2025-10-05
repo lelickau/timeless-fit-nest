dd705f50f41b25b650a589d987e868b29597f573
Elina <lelickau@yandex.ru>
2025-10-05 19:02:07 +0200

    test decorators for queryes

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
 
 @Controller({
   path: 'movies',
@@ -7,5 +16,36 @@ export class MovieController {
   @Get()
   findAll(@Query() query: any) {
     return `Фильмы с параметрами ${JSON.stringify(query)}`;
+  }
+
+  @Get(':id/string/:name')
+  findById(@Param() params: { id: string; name: string }) {
+    return `${params.id} ${params.name}`;
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return headers;
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return { userAgent };
+  }
+
+  @Get('request')
+  getRequestDetails(@Req() request: Request) {
+    return {
+      method: request.method,
+      url: request.url,
+      headers: request.headers,
+      query: request.query,
+      params: request.params,
+    };
+  }
+
+  @Get('response')
+  getResponseDetails(@Res() res: Response) {
+    res.status(201).json({ message: 'Ok' });
   }
 }
