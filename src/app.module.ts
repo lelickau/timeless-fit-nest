2d2f69a04c90894a894c31dc96cd9c63131a0a42
Elina <lelickau@yandex.ru>
2025-10-09 20:53:00 +0200

    added typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { MovieModule } from './modules/movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
@@ -10,17 +11,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        imports: [ConfigModule],
-        type: 'postgres',
-        host: configService.getOrThrow<string>('POSTGRES_HOST'),
-        port: configService.getOrThrow<number>('POSTGRES_PORT'),
-        username: configService.getOrThrow<string>('POSTGRES_USER'),
-        password: configService.getOrThrow<string>('POSTGRES_PASSWORD'),
-        database: configService.getOrThrow<string>('POSTGRES_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
       inject: [ConfigService],
     }),
     MovieModule,
